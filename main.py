01b88541b2df44e77c0dff5050a423e907ad91e7
Leonardo Marques <leo.buldrini@hotmail.com>
2020-07-08 12:44:53 -0300

    error, but finalized part 3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from youtube_statistics import YTstats
+
 #266103494904-qqf6q5h7am12let79mrm1ong26cm1rns.apps.googleusercontent.com
 
 API_KEY = "AIzaSyA5OpjLeWdzFpCHcP6Vbs5Ae6mjIKkm8Rc"
 channel_id = "UCxe05djbFRZRYvTvvppjszA"
 
 yt = YTstats(API_KEY, channel_id)
-#yt.get_channel_statistics()
-#yt.dump()
+yt.get_channel_statistics()
+yt.dump()
 yt.get_channel_video_data()
--- youtube_statistics.py
+++ youtube_statistics.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+from tqdm import tqdm
 
 class YTstats:
     def __init__(self, api_key, channel_id):
@@ -25,8 +25,28 @@ class YTstats:
     def get_channel_video_data(self):
         # get videos ids
         channel_videos = self._get_channel_videos(limit=50)
-        print(channel_videos)
+        print(len(channel_videos))
         # get video statistics
+        parts = ["snippet", "statistics", "contentDetails"]
+        for video_id in tqdm(channel_videos):
+            for part in parts:
+                data = self._get_single_video_data(video_id, part)
+                channel_videos[video_id].update(data)
+
+        self.video_data = channel_videos
+        return channel_videos
+
+    def _get_single_video_data(self, video_id, part):
+        url = f'https://www.googleapis.com/youtube/v3/videos?part={part}&id{video_id}&key{self.api_key}'
+        json_url = requests.get(url)
+        data = json.loads(json_url.text)
+        try:
+            data = data['items'][0][part]
+        except:
+            print('error')
+            data = dict()
+
+        return data
 
     def _get_channel_videos(self, limit=None): 
 
@@ -63,7 +83,6 @@ class YTstats:
                 if kind == 'youtube#video':
                     video_id = item['id']['videoId']
                     channel_videos[video_id] = dict()
-                    print('ok 3')
             except KeyError:
                 print("error")
 
@@ -71,13 +90,17 @@ class YTstats:
     
     
     def dump(self):
-        if self.channel_statistics is None:
+        if self.channel_statistics is None or self.video_data is None:
+            print('video data is none')
             return
+
+        fused_data = {self.channel_id: {"channel_statistics": self.channel_statistics, "video_data": self.video_data}}
+
         
-        channel_title = "Fernando Lopes" #TODO: get channel name from data
+        channel_title = self.video_data.popitem()[1].get('channelTitle', self.channel_id)
         channel_title = channel_title.replace(" ", "_").lower()
         file_name = channel_title + '.json'
         with open(file_name, 'w') as f:
-            json.dump(self.channel_statistics, f, indent=4)
+            json.dump(fused_data, f, indent=4)
 
         print('file dumped')
