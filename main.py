236dd00c683991ff8e8600de9729790527d5bf66
Leonardo Marques <leo.buldrini@hotmail.com>
2020-07-08 16:50:59 -0300

    created funcion

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from youtube_statistics import YTstats
 #266103494904-qqf6q5h7am12let79mrm1ong26cm1rns.apps.googleusercontent.com
 
 API_KEY = "AIzaSyA5OpjLeWdzFpCHcP6Vbs5Ae6mjIKkm8Rc"
-channel_id = "UCK-rDvDp1O1uxxwLUHgt1gQ"
+channel_id = "UCdTWvzwNIeZxSNZ_oWvnBbQ"
 
 def createJSON():
     yt = YTstats(API_KEY, channel_id)
